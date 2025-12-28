e5d03ef39cf2183c274cccf2119ea7beff114141
Zainab Wahab <zainabwahab.eth@gmail.com>
2025-12-28 10:16:58 +0100

    made changes for Render deploy

--- src/routes/match.routes.ts
+++ src/routes/match.routes.ts
@@ -4,9 +4,15 @@ import { MatchController } from "../controllers/match.controller.js";
 const router = Router();
 const matchController = new MatchController();
 
-router.get("/upcoming", matchController.getUpcomingMatches);
-router.get("/matchweek/:matchweek", matchController.getMatchByMatchWeek);
-router.get("/status/:status", matchController.getMatchesByStatus);
+router.get("/upcoming", (req, res) =>
+  matchController.getUpcomingMatches(req, res)
+);
+router.get("/matchweek/:matchweek", (req, res) =>
+  matchController.getMatchByMatchWeek(req, res)
+);
+router.get("/status/:status", (req, res) =>
+  matchController.getMatchesByStatus(req, res)
+);
 router.get("/:id", (req, res) => matchController.getMatchById(req, res));
 
 export default router;
